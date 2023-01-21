e7ee95c7019d2580fc0fc8ff9b400de4c0da72b9
Smal <malyshevs_v@mail.rul>
2023-01-21 23:11:11 +0300

    217 contains duplicate

---
+++ src/test/java/ru/smal/easy/easy217/SolutionTest.java
@@ -0,0 +1,30 @@
+package ru.smal.easy.easy217;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void containsDuplicateTrue() {
+        assertTrue(solution.containsDuplicate(new int[] {1, 1, 2, 3}));
+        assertTrue(solution.containsDuplicate(new int[] {1, 2, 3, 1}));
+        assertTrue(solution.containsDuplicate(new int[] {1, 2, 2, 4}));
+        assertTrue(solution.containsDuplicate(new int[] {1, 2, 3, 4, 4}));
+    }
+
+    @Test
+    void containsDuplicateFalse() {
+        assertFalse(solution.containsDuplicate(new int[] {1, 2, 3, 4}));
+        assertFalse(solution.containsDuplicate(new int[] {5, 23, 4}));
+    }
+}
