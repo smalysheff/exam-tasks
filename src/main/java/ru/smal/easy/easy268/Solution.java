275db61a1cab9a2cec36c1cd0d29e3342363cc85
Smal <malyshevs_v@mail.rul>
2023-01-21 23:45:31 +0300

    268. Missing Number

---
+++ src/main/java/ru/smal/easy/easy268/Solution.java
@@ -0,0 +1,12 @@
+package ru.smal.easy.easy268;
+
+public class Solution {
+
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor;
+    }
+}
---
+++ src/test/java/ru/smal/easy/easy268/SolutionTest.java
@@ -0,0 +1,23 @@
+package ru.smal.easy.easy268;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void missingNumber() {
+        assertEquals(2, solution.missingNumber(new int[] {3, 0, 1}));
+        assertEquals(2, solution.missingNumber(new int[] {0, 1}));
+        assertEquals(8, solution.missingNumber(new int[] {9,6,4,2,3,5,7,0,1}));
+    }
+}
