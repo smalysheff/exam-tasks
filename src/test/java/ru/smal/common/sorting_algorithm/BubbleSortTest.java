0f16a3c320160589bc99c4b71a0363f9ccf3be1a
Smal <malyshevs_v@mail.rul>
2023-01-28 21:35:48 +0300

    136. Single Number

--- src/test/java/ru/smal/common/sorting_algorithm/BubbleSortTest.java
+++ src/test/java/ru/smal/common/sorting_algorithm/BubbleSortTest.java
@@ -4,7 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class SolutionTest {
+class BubbleSortTest {
 
     private BubbleSort bubbleSort;
 
