70fe855534b516beec6bff3f09dcc47d44c5ad44
Smal <malyshevs_v@mail.rul>
2023-01-28 23:22:47 +0300

    stream api max min

---
+++ src/main/java/ru/smal/common/stream_api/max_min/Solution.java
@@ -0,0 +1,52 @@
+package ru.smal.common.stream_api.max_min;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    private static final String OBJECT_NOT_FOUND = "Object not found";
+
+    public Person findByMaxDate(List<Person> people) {
+        return people
+                .stream()
+                .max(Comparator.comparing(Person::birthday))
+                .orElseThrow(() -> new RuntimeException(OBJECT_NOT_FOUND));
+    }
+
+    public Person findByMaxSalaryWithMethodReference(List<Person> people) {
+        return people
+                .stream()
+                .max(Comparator.comparingInt(Person::salary))
+                .orElseThrow(() -> new RuntimeException(OBJECT_NOT_FOUND));
+    }
+
+    public Person findByMaxSalaryWithLambda(List<Person> people) {
+        return people
+                .stream()
+                .max((x, y) -> x.salary() - y.salary())
+//                .max((x, y) -> Integer.compare(x.salary(), y.salary()))
+                .orElseThrow(() -> new RuntimeException(OBJECT_NOT_FOUND));
+    }
+
+    public Person findByMaxSalaryUsingCollect(List<Person> people) {
+        return people
+                .stream()
+                .collect(Collectors.maxBy((x, y) -> x.salary() - y.salary()))
+//                .collect(Collectors.maxBy(Comparator.comparing(Person::salary)))
+                .orElseThrow(() -> new RuntimeException(OBJECT_NOT_FOUND));
+    }
+
+    public Person findByMaxSalaryUsingReduce(List<Person> people) {
+        return people
+                .stream()
+                .reduce((x, y) -> x.salary() > y.salary() ? x : y)
+                .orElseThrow(() -> new RuntimeException(OBJECT_NOT_FOUND));
+    }
+
+}
+
+record Person(LocalDate birthday, int salary) {
+}
---
+++ src/test/java/ru/smal/common/stream_api/max_min/SolutionTest.java
@@ -0,0 +1,87 @@
+package ru.smal.common.stream_api.max_min;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void findByMaxDate() {
+        List<Person> people = buildPersons();
+        Person actual = solution.findByMaxDate(people);
+
+        Assertions
+                .assertThat(actual.birthday())
+                .isEqualTo(LocalDate.of(2001, 9, 2));
+    }
+
+    @Test
+    void findByMaxDateWithEmptyList() {
+        Assertions
+                .assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> solution.findByMaxDate(Collections.emptyList()))
+                .withMessage("Object not found");
+    }
+
+
+    @Test
+    void findByMaxSalaryWithMethodReference() {
+        List<Person> people = buildPersons();
+        Person actual = solution.findByMaxSalaryWithMethodReference(people);
+
+        Assertions
+                .assertThat(actual.salary())
+                .isEqualTo(44);
+    }
+
+    @Test
+    void findByMaxSalaryWithLambda() {
+        List<Person> people = buildPersons();
+        Person actual = solution.findByMaxSalaryWithLambda(people);
+
+        Assertions
+                .assertThat(actual.salary())
+                .isEqualTo(44);
+    }
+
+    @Test
+    void findByMaxSalaryUsingCollect() {
+        List<Person> people = buildPersons();
+        Person actual = solution.findByMaxSalaryUsingCollect(people);
+
+        Assertions
+                .assertThat(actual.salary())
+                .isEqualTo(44);
+    }
+
+    @Test
+    void findByMaxSalaryUsingReduce() {
+        List<Person> people = buildPersons();
+        Person actual = solution.findByMaxSalaryUsingReduce(people);
+
+        Assertions
+                .assertThat(actual.salary())
+                .isEqualTo(44);
+    }
+
+    private List<Person> buildPersons() {
+        return List.of(
+                new Person(LocalDate.of(2000, 8, 1), 12),
+                new Person(LocalDate.of(2001, 9, 2), 44),
+                new Person(LocalDate.of(1990, 10, 3), 38)
+        );
+    }
+}
