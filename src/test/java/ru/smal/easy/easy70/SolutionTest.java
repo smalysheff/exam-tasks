9d8dae8059bc0e75e82ab8422ccf8ff23d717215
Sergey Malyshev <malyshevs_v@mail.rul>
2023-02-07 22:36:30 +0300

    70. Climbing Stairs

---
+++ src/test/java/ru/smal/easy/easy70/SolutionTest.java
@@ -0,0 +1,32 @@
+package ru.smal.easy.easy70;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class SolutionTest {
+
+    @InjectMocks
+    private Solution solution;
+
+    @Test
+    void climbStairsRecursion() {
+        assertEquals(1, solution.climbStairsRecursion(1));
+        assertEquals(2, solution.climbStairsRecursion(2));
+        assertEquals(3, solution.climbStairsRecursion(3));
+        assertEquals(8, solution.climbStairsRecursion(5));
+    }
+
+    @Test
+    void climbStairsForLoop() {
+        assertEquals(1, solution.climbStairsForLoop(1));
+        assertEquals(2, solution.climbStairsForLoop(2));
+        assertEquals(3, solution.climbStairsForLoop(3));
+        assertEquals(8, solution.climbStairsForLoop(5));
+        assertEquals(1836311903, solution.climbStairsForLoop(45));
+    }
+}
