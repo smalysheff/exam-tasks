d5dcf26273643092659802d790c606432f9fa553
Sergey Malyshev <malyshevs_v@mail.rul>
2023-02-08 20:55:22 +0300

    refactoring

--- src/main/java/ru/smal/easy/easy217/Solution.java
+++ src/main/java/ru/smal/easy/easy217/Solution.java
@@ -5,16 +5,16 @@ import java.util.Set;
 
 /**
  * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
- *
+ * <p>
  * Example 1:
  * Input: nums = [1,2,3,1]
  * Output: true
- *
- *  Example 2:
+ * <p>
+ * Example 2:
  * Input: nums = [1,2,3,4]
  * Output: false:
- *
- *  Example 3:
+ * <p>
+ * Example 3:
  * Input: nums = [1,1,1,3,3,4,3,2,4,2,2]
  * Output: true
  */
